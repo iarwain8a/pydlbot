39f4d6916244d0d6ab6242386bd01cf348c08d84
iarwain8a <iarwain@hotmail.com.ar>
2016-02-19 00:13:00 -0300

    pyqt5
    implementacion de la interface

--- pruebas.py
+++ pruebas.py
@@ -10,11 +10,11 @@ import os
 #print (soup2.prettify())
 #search/the%20flash%20s01e01/0/7/0 el 7 es el parametro para ordenar por seeders.
 
-soup2 = httpscrap.request_soup("https://thepiratebay.se/search/the%20flash%20s01e01/0/7/0")
+soup2 = httpscrap.request_soup("https://thepiratebay.se/search/the%20flash%20s02e01/0/7/0")
 magnet = parse.get_magnet_link(str(httpscrap.get_tag('a','title','Download this torrent using magnet',soup2)))
 #magnet = httpscrap.get_tag('a','title','Download this torrent using magnet',soup2)
 
-#print (magnet)
 os.startfile(magnet)
+
 
 
---
+++ pydlbot_ui.py
@@ -0,0 +1,63 @@
+# -*- coding: utf-8 -*-
+
+# Form implementation generated from reading ui file 'C:\Users\iarwa\Documents\GitHub\pydlbot\pydlbot.ui'
+#
+# Created by: PyQt5 UI code generator 5.5.1
+#
+# WARNING! All changes made in this file will be lost!
+
+from PyQt5 import QtCore, QtGui, QtWidgets
+import sys
+from PyQt5.QtWidgets import QApplication, QWidget
+
+
+class Ui_Dialog():
+    def setupUi(self, Dialog):
+        Dialog.setObjectName("Dialog")
+        Dialog.resize(640, 480)
+        self.gridLayout = QtWidgets.QGridLayout(Dialog)
+        self.gridLayout.setObjectName("gridLayout")
+        self.verticalLayout = QtWidgets.QVBoxLayout()
+        self.verticalLayout.setObjectName("verticalLayout")
+        self.label = QtWidgets.QLabel(Dialog)
+        self.label.setObjectName("label")
+        self.verticalLayout.addWidget(self.label)
+        self.horizontalLayout = QtWidgets.QHBoxLayout()
+        self.horizontalLayout.setObjectName("horizontalLayout")
+        self.lineEdit = QtWidgets.QLineEdit(Dialog)
+        self.lineEdit.setObjectName("lineEdit")
+        self.horizontalLayout.addWidget(self.lineEdit)
+        self.pushButton = QtWidgets.QPushButton(Dialog)
+        self.pushButton.setObjectName("pushButton")
+        self.horizontalLayout.addWidget(self.pushButton)
+        self.verticalLayout.addLayout(self.horizontalLayout)
+        self.gridLayout.addLayout(self.verticalLayout, 0, 0, 1, 1)
+        self.buttonBox = QtWidgets.QDialogButtonBox(Dialog)
+        self.buttonBox.setOrientation(QtCore.Qt.Horizontal)
+        self.buttonBox.setStandardButtons(QtWidgets.QDialogButtonBox.Cancel|QtWidgets.QDialogButtonBox.Ok)
+        self.buttonBox.setObjectName("buttonBox")
+        self.gridLayout.addWidget(self.buttonBox, 1, 0, 1, 1)
+
+        self.retranslateUi(Dialog)
+        self.buttonBox.accepted.connect(Dialog.accept)
+        self.buttonBox.rejected.connect(Dialog.reject)
+        self.pushButton.clicked.connect(self.label.clear)
+        self.lineEdit.textEdited['QString'].connect(self.label.setText)
+        self.pushButton.clicked.connect(self.lineEdit.clear)
+        QtCore.QMetaObject.connectSlotsByName(Dialog)
+
+    def retranslateUi(self, Dialog):
+        _translate = QtCore.QCoreApplication.translate
+        Dialog.setWindowTitle(_translate("Dialog", "Dialog"))
+        self.label.setText(_translate("Dialog", "hello world"))
+        self.pushButton.setText(_translate("Dialog", "Clear"))
+
+
+
+if __name__ == '__main__':
+    app = QtWidgets.QApplication(sys.argv)
+    Dialog = QtWidgets.QDialog()
+    ui = Ui_Dialog()
+    ui.setupUi(Dialog)
+    Dialog.show()
+    sys.exit(app.exec_())
